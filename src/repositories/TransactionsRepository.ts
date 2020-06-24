673c2288b4720bd47e484b9d1b228a661f1dccaa
Eloilson Rocha <eloilson.ageedu@gmail.com>
2020-06-24 13:03:50 -0300

    Implementado as funcionaldades de create, list, e validation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +15,41 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (totalIncome, { value, type }) =>
+        type === 'income' ? totalIncome + value : totalIncome,
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (totalOutcome, { value, type }) =>
+        type === 'outcome' ? totalOutcome + value : totalOutcome,
+      0,
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
